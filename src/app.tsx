f6fa5a4a0d807dbf1049cb1c0d0807dc8aecf696
Egor Chernyshev <egorch.formal@gmail.com>
2021-02-27 23:52:09 +0000

    added markup for new message

--- src/app.tsx
+++ src/app.tsx
@@ -1,83 +1,101 @@
-import * as React from 'react';
-import { render } from 'react-dom';
+import * as React from "react";
+import {render} from "react-dom";
 let root = document.querySelector("#root");
-let messages = [{
+let messages = [
+  {
     time: "23:55:23",
     username: "Vladiak",
-    message_text: "Hello world"
-}, {
+    message_text: "Hello world",
+  },
+  {
     time: "12:06:12",
     username: "Egorcik",
-    message_text: "I am the best jhin in EU"
-},
-{
-    time: "13:12:46",
-    username: "Mia boyka",
-    message_text: `
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac nulla iaculis, aliquet lacus sit amet, congue odio. Nullam pharetra sapien a efficitur fermentum. Fusce sit amet volutpat ante. Vestibulum ac risus fermentum ex laoreet pharetra ornare at leo. Vestibulum posuere, dolor at tincidunt fermentum, tortor purus commodo dui, et vehicula velit ligula at risus. Aliquam non tristique sapien, hendrerit tristique neque. Sed tincidunt sed diam et pharetra. Proin vel lorem volutpat, faucibus risus lobortis, tincidunt velit. Quisque mauris turpis, iaculis id eros at, consequat gravida nibh.
+    message_text: "I am the best jhin in EU",
+  },
+];
 
-Quisque nec ullamcorper urna. Maecenas id nisi viverra augue porttitor iaculis. Cras lobortis nisi ut risus pretium egestas. Maecenas hendrerit erat lectus, volutpat tempor justo gravida in. Fusce varius nisl arcu, luctus ullamcorper ante pulvinar facilisis. In congue dictum tempus. Vivamus non nunc eleifend, ultrices mi ut, consectetur lectus. Donec vehicula, enim at condimentum semper, est metus ultrices erat, sit amet mattis dui purus eu felis. Vivamus pretium risus vel commodo ornare. Quisque dapibus ut justo non ultrices. Donec dapibus sapien sit amet semper scelerisque. Nullam pretium sagittis varius.
-    `
-}]
-
-
-class Message extends React.Component{
-    message: any;
-    constructor(props){
-        super(props);
-        this.message = this.props.message;
-    }
-    render(){
-        return (
-            <div className="message_container">
-                <div className="message_metadata">
-                    <span className="message_username">{this.message.username}</span>
-                    <span className="message_time">{this.message.time}</span>
-                </div>
-                <div className="message_text_container">
-                    <span className="message_text">{this.message.message_text}</span>
-                </div>
-            </div>
-        )
-    }
+class Message extends React.Component {
+  message: any;
+  constructor(props) {
+    super(props);
+    this.message = this.props.message;
+  }
+  render() {
+    return (
+      <div className="message_container">
+        <div className="message_metadata">
+          <span className="message_username">{this.message.username}</span>
+          <span className="message_time">{this.message.time}</span>
+        </div>
+        <div className="message_text_container">
+          <span className="message_text">{this.message.message_text}</span>
+        </div>
+      </div>
+    );
+  }
 }
 
+class New_message_menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message_text: "",
+      is_text_area_focused: false
+    };
+  }
 
-class Message_container extends React.Component{
-    messages: any;
-    markup: any;
-    constructor(props){
-        super(props);
-        this.messages = this.props.messages;
-        this.markup = this.messages.map((message, index) => {
-            return (
-                <Message message={message} key={index}>
+  on_textarea_change_focus = (bool) => {
+    this.setState({
+        is_text_area_focused: bool
+    })
+  }
 
-                </Message>
-            )
-        })
+  on_textarea_value_change = () => {
+
+  }
+
+  render() {
+    let classes = "new_message_container";
+    if(this.state.is_text_area_focused === true){
+        classes += " focused";
     }
-    render(){
-        return (
-            <div className="conversation_container">
-                {this.markup}
-            </div>
-        )
-    }
+    return (
+      <div className={classes}>
+        <textarea placeholder="Type new message here..." spellCheck="false" autoComplete="false" onFocus={() => {this.on_textarea_change_focus(true)}} onBlur={() => {this.on_textarea_change_focus(false)}} value={this.state.message_text} onChange={this.on_textarea_value_change}></textarea>
+      </div>
+    );
+  }
 }
 
-
-class App extends React.Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        return (
-            <Message_container messages={messages}>
-            </Message_container>
-        )
-    }
+class Conversation_container extends React.Component {
+  messages: any;
+  markup: any;
+  constructor(props) {
+    super(props);
+    this.messages = this.props.messages;
+    this.markup = this.messages.map((message, index) => {
+      return <Message message={message} key={index}></Message>;
+    });
+  }
+  render() {
+    return (
+      <div className="conversation_container">
+        <div className="messages_container">{this.markup}</div>
+        <New_message_menu></New_message_menu>
+      </div>
+    );
+  }
 }
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Conversation_container messages={messages}></Conversation_container>
+    );
+  }
+}
 
-render(<App/>, root);
+render(<App />, root);
